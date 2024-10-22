49f52084487aa0a6ddc311d5dfa03d14771b4901
Lord225 <zlotymaciej@gmail.com>
2024-10-22 16:23:49 +0200

    extract data

--- fetch-data/extract_data_from_ziped_repos.py
+++ fetch-data/extract_data_from_ziped_repos.py
@@ -1,27 +1,28 @@
 from pathlib import Path
-from zipfile import ZipFile
-
+from zipfile import ZipFile, BadZipFile
+import tqdm 
 PYTHON_EXTENSIONS = [ ".py" ]
 size_sum = 0
 zips = {} 
 path = Path('./downloaded_repos/')
-for file in path.glob('*.zip'):
-    with ZipFile(file, 'r') as zipObj:
-        for entry in zipObj.infolist():
-            if Path(entry.filename).suffix in PYTHON_EXTENSIONS:
-                print(entry.filename, ' : ', entry.file_size, ' : ')
-                size_sum += entry.file_size
-                # store filename and file content in directory
-                zips[entry.filename] = zipObj.read(entry.filename).decode('utf-8')
 
 
-# coount total amount od def in all files
+iterator = tqdm.tqdm(path.glob('*.zip'), desc='Processing ZIP files', unit='files')
+
+for file in iterator:
+    try:
+        with ZipFile(file, 'r') as zipObj:
+            for entry in zipObj.infolist():
+                if Path(entry.filename).suffix in PYTHON_EXTENSIONS:
+                    size_sum += entry.file_size
+                    # store filename and file content in directory
+                    zips[entry.filename] = zipObj.read(entry.filename).decode('utf-8', errors='ignore')
+    except BadZipFile:
+        print(f"Warning: {file} is not a zip file and will be skipped.")
 
 def_count = 0
 for file in zips:
     def_count += zips[file].count('def ')
-
-
 
 print('total files:', len(zips))
 print('size:', size_sum / (1024 * 1024))
